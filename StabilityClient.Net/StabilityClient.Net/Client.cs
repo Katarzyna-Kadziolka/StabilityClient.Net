f3ee5aedbadac1e777d1d83becbc319aef5771c8
Katarzyna <kasiakadziolka@gmail.com>
2022-11-15 19:38:55 +0100

    remove submodules, add proto links

--- StabilityClient.Net/StabilityClient.Net/Client.cs
+++ StabilityClient.Net/StabilityClient.Net/Client.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Protobuf.WellKnownTypes;
+using Gooseai;
 using Grpc.Net.Client;
 
 namespace StabilityClient.Net;
@@ -6,5 +7,6 @@ namespace StabilityClient.Net;
 public class Client  {
     public Client() {
         using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+        var gen = new DashboardService.DashboardServiceClient(channel);
     }
 }

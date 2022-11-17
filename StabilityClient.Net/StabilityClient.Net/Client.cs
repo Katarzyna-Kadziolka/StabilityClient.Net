153a7c825b94dbf6ed7de653c901f206c7f3dcae
Katarzyna <kasiakadziolka@gmail.com>
2022-11-17 11:45:14 +0100

    Add lazy props

--- StabilityClient.Net/StabilityClient.Net/Client.cs
+++ StabilityClient.Net/StabilityClient.Net/Client.cs
@@ -1,12 +1,23 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using Gooseai;
+﻿using Gooseai;
 using Grpc.Net.Client;
 
 namespace StabilityClient.Net;
 
 public class Client  {
+    private readonly GrpcChannel channel;
+    private DashboardService.DashboardServiceClient? _dashboard;
+    private EnginesService.EnginesServiceClient? _engines;
+    private GenerationService.GenerationServiceClient? _generation;
+    private ProjectService.ProjectServiceClient? _project;
+
     public Client() {
-        using var channel = GrpcChannel.ForAddress("https://localhost:7042");
-        var gen = new DashboardService.DashboardServiceClient(channel);
+        channel = GrpcChannel.ForAddress("https://localhost:7042");
     }
+
+    public DashboardService.DashboardServiceClient Dashboard => _dashboard ??= new DashboardService.DashboardServiceClient(channel);
+    public EnginesService.EnginesServiceClient Engines => _engines ??= new EnginesService.EnginesServiceClient(channel);
+    public GenerationService.GenerationServiceClient Generation =>
+        _generation ??= new GenerationService.GenerationServiceClient(channel);
+    public ProjectService.ProjectServiceClient Project => _project ??= new ProjectService.ProjectServiceClient(channel);
 }
+

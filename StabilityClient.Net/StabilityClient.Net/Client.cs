6bb0e452415753ab067a8a1ef765e2bf3cd4d215
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2022-11-17 10:35:20 +0100

    Add  protos files (#2)
    * Added the submodule to the project
    
    * Remove submodules
    
    * remove submodules
    
    * Delete .gitmodules
    
    * add submodules
    
    * remove submodules, add proto links
    
    * idea files
    
    * remove gitmodules file
    
    * change gitignore
    
    * update gitignore

--- StabilityClient.Net/StabilityClient.Net/Client.cs
+++ StabilityClient.Net/StabilityClient.Net/Client.cs
@@ -1,5 +1,12 @@
-﻿namespace StabilityClient.Net;
+﻿using Google.Protobuf.WellKnownTypes;
+using Gooseai;
+using Grpc.Net.Client;
 
-public class Client {
-    
+namespace StabilityClient.Net;
+
+public class Client  {
+    public Client() {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+        var gen = new DashboardService.DashboardServiceClient(channel);
+    }
 }

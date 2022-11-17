192e4840f0ac7050d561e8954b9d1172dbef1e59
Katarzyna <kasiakadziolka@gmail.com>
2022-11-17 12:09:46 +0100

    Init unit tests projectc

---
+++ StabilityClient.Net/StabilityClient.Net.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TestProject1StabilityClient.Net.Tests.Unit;
+
+public class Tests {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+}

e1578e792759771a7b4e9d90215a8ce1a757acf1
Katarzyna <kasiakadziolka@gmail.com>
2023-01-04 16:10:54 +0100

    Add comments

--- StabilityClient.Net/StabilityClient.Net/RequestBuilder.cs
+++ StabilityClient.Net/StabilityClient.Net/RequestBuilder.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Gooseai;
 
 namespace StabilityClient.Net;
@@ -7,6 +8,8 @@ public class RequestBuilder {
     public const ulong DefaultImageHeight = 512;
     public const ulong DefaultImageWidth = 512;
     public const ulong DefaultImageSteps = 30;
+    public const ulong DefaultNumberOfSamples = 1;
+
     
     private readonly Request _request = new () {
         EngineId = DefaultEngineId,
@@ -14,9 +17,14 @@ public class RequestBuilder {
             Height = DefaultImageHeight,
             Width = DefaultImageWidth,
             Steps = DefaultImageSteps,
+            Samples = DefaultNumberOfSamples
         }
     };
-
+    /// <summary>
+    /// Creates a request based on previously specified parameters
+    /// </summary>
+    /// <returns>Request</returns>
+    /// <exception cref="ArgumentException">throw when text prompt is null or empty</exception>
     public Request Build() {
         if (_request.Prompt.Count==0) {
             throw new ArgumentException($"Prompt cannot be empty; Use {nameof(SetTextPrompt)} to add new prompt.");
@@ -25,26 +33,47 @@ public class RequestBuilder {
         return _request;
     }
 
-    public RequestBuilder SetHeight(ulong height) {
+    /// <summary>
+    /// Set the expected height of the image
+    /// </summary>
+    /// <param name="height">expected value of image height; must be a multiple of 64</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageHeight(ulong height) {
         _request.Image.Height = height;
         return this;
     }
-
-    public RequestBuilder SetWidth(ulong width) {
+    /// <summary>
+    /// Set the expected width of the image
+    /// </summary>
+    /// <param name="width">expected value of image width; must be a multiple of 64</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageWidth(ulong width) {
         _request.Image.Width = width;
         return this;
     }
-
-    public RequestBuilder SetSteps(ulong steps) {
+    /// <summary>
+    ///  Set the number of steps to spend generation (diffusing) image
+    /// </summary>
+    /// <param name="steps">expected number of stpes</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSteps(ulong steps) {
         _request.Image.Steps = steps;
         return this;
     }
-
+    /// <summary>
+    /// Sets which engine will take care of image generation 
+    /// </summary>
+    /// <param name="engineId">expected engine id</param>
+    /// <returns>RequestBuilder</returns>
     public RequestBuilder SetEngineId(string engineId) {
         _request.EngineId = engineId;
         return this;
     }
-
+    /// <summary>
+    /// Sets the text prompt, based on which the image will be generated.
+    /// </summary>
+    /// <param name="text">expected prompt</param>
+    /// <returns>RequestBuilder</returns>
     public RequestBuilder SetTextPrompt(string text) {
         _request.Prompt.Clear();
         _request.Prompt.Add(new Prompt {
@@ -55,10 +84,23 @@ public class RequestBuilder {
         });
         return this;
     }
-
-    public RequestBuilder SetSeed(uint seed) {
+    /// <summary>
+    /// Sets the seed for the random number generator
+    /// </summary>
+    /// <param name="seed">number for random number generator</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSeed(uint seed) {
         _request.Image.Seed.Clear();
         _request.Image.Seed.Add(seed);
+        return this;
+    }
+    /// <summary>
+    /// Sets the number of images to generate
+    /// </summary>
+    /// <param name="samples">number of images</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSamples(ulong samples) {
+        _request.Image.Samples = samples;
         return this;
     }
 }

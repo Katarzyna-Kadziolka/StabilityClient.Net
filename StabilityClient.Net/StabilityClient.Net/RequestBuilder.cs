7bc727e81b5cebac6e8f13c0619788c3e47aca04
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2023-01-07 10:31:34 +0100

    Generator utilities (#4)
    * Add request builder
    
    * Update proto files
    
    * Add comments
    
    * Tests for request generator
    
    * Remove unused using
    
    * format

---
+++ StabilityClient.Net/StabilityClient.Net/RequestBuilder.cs
@@ -0,0 +1,112 @@
+using Gooseai;
+
+namespace StabilityClient.Net;
+
+public class RequestBuilder {
+    public const string DefaultEngineId = "stable-diffusion-512-v2-1";
+    public const ulong DefaultImageHeight = 512;
+    public const ulong DefaultImageWidth = 512;
+    public const ulong DefaultImageSteps = 30;
+    public const ulong DefaultNumberOfSamples = 1;
+
+
+    private readonly Request _request = new() {
+        EngineId = DefaultEngineId,
+        Image = new ImageParameters {
+            Height = DefaultImageHeight,
+            Width = DefaultImageWidth,
+            Steps = DefaultImageSteps,
+            Samples = DefaultNumberOfSamples
+        }
+    };
+
+    /// <summary>
+    /// Creates a request based on previously specified parameters
+    /// </summary>
+    /// <returns>Request</returns>
+    /// <exception cref="ArgumentException">throw when text prompt is null or empty</exception>
+    public Request Build() {
+        if (_request.Prompt.Count == 0) {
+            throw new ArgumentException($"Prompt cannot be empty; Use {nameof(SetTextPrompt)} to add new prompt.");
+        }
+
+        return _request;
+    }
+
+    /// <summary>
+    /// Set the expected height of the image
+    /// </summary>
+    /// <param name="height">expected value of image height; must be a multiple of 64</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageHeight(ulong height) {
+        _request.Image.Height = height;
+        return this;
+    }
+
+    /// <summary>
+    /// Set the expected width of the image
+    /// </summary>
+    /// <param name="width">expected value of image width; must be a multiple of 64</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageWidth(ulong width) {
+        _request.Image.Width = width;
+        return this;
+    }
+
+    /// <summary>
+    ///  Set the number of steps to spend generation (diffusing) image
+    /// </summary>
+    /// <param name="steps">expected number of steps</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSteps(ulong steps) {
+        _request.Image.Steps = steps;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets which engine will take care of image generation 
+    /// </summary>
+    /// <param name="engineId">expected engine id</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetEngineId(string engineId) {
+        _request.EngineId = engineId;
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the text prompt, based on which the image will be generated.
+    /// </summary>
+    /// <param name="text">expected prompt</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetTextPrompt(string text) {
+        _request.Prompt.Clear();
+        _request.Prompt.Add(new Prompt {
+            Text = text,
+            Parameters = new PromptParameters {
+                Init = true
+            }
+        });
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the seed for the random number generator
+    /// </summary>
+    /// <param name="seed">number for random number generator</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSeed(uint seed) {
+        _request.Image.Seed.Clear();
+        _request.Image.Seed.Add(seed);
+        return this;
+    }
+
+    /// <summary>
+    /// Sets the number of images to generate
+    /// </summary>
+    /// <param name="samples">number of images</param>
+    /// <returns>RequestBuilder</returns>
+    public RequestBuilder SetImageSamples(ulong samples) {
+        _request.Image.Samples = samples;
+        return this;
+    }
+}
--- StabilityClient.Net/StabilityClient.Net/StabilityClient.cs
+++ StabilityClient.Net/StabilityClient.Net/StabilityClient.cs
@@ -6,6 +6,7 @@ namespace StabilityClient.Net;
 
 public class StabilityClient : IStabilityClient {
     private const string ApiKeyVariableName = "STABILITY_KEY";
+
     public DashboardService.DashboardServiceClient Dashboard =>
         _dashboard ??= new DashboardService.DashboardServiceClient(_channel);
 
@@ -18,10 +19,15 @@ public class StabilityClient : IStabilityClient {
     public ProjectService.ProjectServiceClient Project =>
         _project ??= new ProjectService.ProjectServiceClient(_channel);
 
+    /// <summary>
+    /// Creates an instance of the StabilityClient class based on the given apiKey and host (optional)
+    /// </summary>
+    /// <param name="apiKey">enables authorization</param>
+    /// <param name="host">optional, address to API</param>
     public StabilityClient(string apiKey, string host = "https://grpc.stability.ai:443") {
         var grpcChannelOptions = new GrpcChannelOptions() {
             Credentials = ChannelCredentials.Create(ChannelCredentials.SecureSsl,
-                CallCredentials.FromInterceptor((context, metadata) => {
+                CallCredentials.FromInterceptor((_, metadata) => {
                     metadata.Add("Authorization", $"Bearer {apiKey}");
                     return Task.CompletedTask;
                 })
@@ -30,8 +36,11 @@ public class StabilityClient : IStabilityClient {
 
         _channel = GrpcChannel.ForAddress(host, grpcChannelOptions);
     }
-
-    public StabilityClient() : this(GetApiKeyFromEnv()) { }
+    /// <summary>
+    /// Creates an instance of the StabilityClient class based on the environment variable STABILITY_KEY
+    /// </summary>
+    public StabilityClient() : this(GetApiKeyFromEnv()) {
+    }
 
     private static string GetApiKeyFromEnv() {
         var apiKey = Environment.GetEnvironmentVariable(ApiKeyVariableName);
@@ -41,7 +50,7 @@ public class StabilityClient : IStabilityClient {
 
         return apiKey;
     }
-    
+
     private readonly GrpcChannel _channel;
 
     private DashboardService.DashboardServiceClient? _dashboard;
